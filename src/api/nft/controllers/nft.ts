f9d552d21a0323c735ad69f9f84499e77bd74e73
JHSeo <qnfqnfqnf@gmail.com>
2022-05-22 15:57:22 +0900

    add collection, nft routes

---
+++ src/api/nft/controllers/nft.ts
@@ -0,0 +1,7 @@
+/**
+ *  nft controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::nft.nft');
---
+++ src/api/nft/routes/nft.ts
@@ -0,0 +1,7 @@
+/**
+ * nft router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::nft.nft');
