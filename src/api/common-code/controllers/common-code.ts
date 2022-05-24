3519a827f00fd7c828acecd786a0e159136eb192
JHSeo <qnfqnfqnf@gmail.com>
2022-05-24 23:20:00 +0900

    update strapi-collections
    - update collection table
    - add common-code table
    - add nft table

---
+++ src/api/common-code/controllers/common-code.ts
@@ -0,0 +1,7 @@
+/**
+ *  common-code controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::common-code.common-code');
---
+++ src/api/common-code/routes/common-code.ts
@@ -0,0 +1,7 @@
+/**
+ * common-code router.
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::common-code.common-code');
